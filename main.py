6068bfd3daebbf9c02db5e7474283065ec2e45dc
grzlz <guillermo.rdzlpz@gmail.com>
2023-06-06 14:38:12 -0500

    use chains to get user input

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import os
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
+from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
-
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
-# llm = OpenAI(temperature=0.9, openai_api_key=API_KEY)
 # text = "What would be a good company name for a company that makes artificial intelligence fine tuning solutions as a service?"
 # print(llm(text))
 
+llm = OpenAI(temperature=0.9, openai_api_key=API_KEY)
 prompt = PromptTemplate(
-    template="What is a good name for a company that makes {product}",
-    input_variables=["product"]
+    input_variables=["product"],
+    template="What is a good name for a company that makes {product}"
 )
 
-print(prompt.format(product="colorful socks"))
+chain = LLMChain(llm=llm, prompt=prompt)
+print(chain.run("colorful socks"))

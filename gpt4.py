5d7d3401d8529d98392a811b10edae7de8fdcc20
grzlz <guillermo.rdzlpz@gmail.com>
2023-06-07 12:49:41 -0500

    recharge with new set of reviews

--- gpt4.py
+++ gpt4.py
@@ -33,25 +33,53 @@ def get_sentiment(review):
 
 # Define a function to process reviews
 def process_reviews(reviews):
-    results = [get_sentiment(review) for review in reviews]
-    for i in results:
-        print(i)
-    return results
+    for review in reviews:
+        print(get_sentiment(review))
+    # results = [get_sentiment(review) for review in reviews]
+    # for i in results:
+    #     print(i)
+    # return results
 
 reviews = [
-    "I bought apples from Walmart and they were rotten. Very disappointed.",
-    "The kiwis I got from Walmart were not fresh at all. I won't be buying from them again.",
-    "Walmart's bananas were overripe. Not happy with my purchase.",
-    "The mangos from Walmart were bruised and inedible. This is unacceptable.",
-    "I am extremely angry about the coconuts I bought from Walmart. They were moldy!",
-    "I bought apples from Atlante and they were the best I've ever had. Very happy with my purchase.",
-    "The kiwis I got from Atlante were perfectly ripe. I'll definitely buy from them again.",
-    "Atlante's bananas were delicious. Very satisfied.",
-    "The mangos from Atlante were sweet and juicy. Excellent quality.",
-    "I am extremely happy about the coconuts I bought from Atlante. They were fresh and tasty!"
+    "I bought apples from Walmart. They were rotten. I'm very disappointed.",
+    "I got kiwis from Superama. They were not fresh at all. I won't buy from them again.",
+    "I purchased bananas from Bodega Aurrera. They were overripe. I'm not happy.",
+    "I bought mangos from Walmart. They were bruised. I'm very upset.",
+    "I got coconuts from Superama. They were moldy. I'm extremely angry.",
+
+    "I bought apples from Walmart. They were okay, but not the best I've had.",
+    "I got kiwis from Superama. They were fine, but I've had better.",
+    "I purchased bananas from Bodega Aurrera. They were decent, but not great.",
+    "I bought mangos from Walmart. They were average, nothing special.",
+    "I got coconuts from Superama. They were satisfactory, but not outstanding.",
+
+    "I bought apples from Bodega Aurrera. They were good, but not the best I've had.",
+    "I got kiwis from Walmart. They were nice, but I've had better.",
+    "I purchased bananas from Superama. They were tasty, but not great.",
+    "I bought mangos from Bodega Aurrera. They were delicious, nothing special.",
+    "I got coconuts from Walmart. They were yummy, but not outstanding.",
+
+    "I bought apples from Superama. They were delicious. I'm very happy.",
+    "I got kiwis from Bodega Aurrera. They were very fresh. I'm extremely satisfied.",
+    "I purchased bananas from Walmart. They were excellent. I'm very pleased.",
+    "I bought mangos from Superama. They were fantastic. I'm extremely happy.",
+    "I got coconuts from Bodega Aurrera. They were perfect. I'm very delighted."
 ]
 
+
 process_reviews(reviews)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

555226b1f6b47ae54068f360bc8674204487e921
grzlz <guillermo.rdzlpz@gmail.com>
2023-06-07 18:45:43 -0500

    create uploaded_data global variable

--- backend.py
+++ backend.py
@@ -1,12 +1,29 @@
+import io
+import csv
 import uvicorn
-from fastapi import FastAPI
+import pandas as pd
+from reviews import reviews
 from gpt4 import process_reviews
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
+uploaded_data = []
+
+@app.post("/uploadcsv/")
+async def upload_csv(file: UploadFile):
+    contents = await file.read()
+    data = contents.decode('utf-8').splitlines()
+    reader = csv.reader(data)
+    next(reader)  # Skip the header
+    reviews = [row[0] for row in reader]  # Assuming 'review' is the first column
+    print(reviews)
+    # Now you can process the list 'reviews' as needed
+    return {"filename": file.filename}
+
 @app.get('/data')
 def get_data():
-    process_reviews(reviews=)
+    data = process_reviews(reviews)
     return data
 
 uvicorn.run(app, host='localhost', port=5000)
